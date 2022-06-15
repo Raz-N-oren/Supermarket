04ddbe1c41cafd32c73645e2cc87df5ae5479fa5
Raz-N-oren <razoren2394@gmail.com>
2022-06-15 17:35:55 +0300

    final login

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { combineLatest } from 'rxjs';
 import IUser from 'src/app/models/IUser.model';
+import UserLoginData from 'src/app/models/UserLoginData.model';
+import { CartsService } from 'src/app/services/carts.service';
 import { StateService } from 'src/app/services/state.service';
 import { UsersService } from 'src/app/services/users.service';
 
@@ -11,17 +15,36 @@ import { UsersService } from 'src/app/services/users.service';
 })
 export class LoginComponent implements OnInit {
 
-  userName: string = '';
-  password: string = '';
+  loginUserData: UserLoginData = {userEmail: "", password: ""};
+  isLoginFail: boolean = false;
+  userLoginForm: FormGroup;
 
-  constructor(private usersService: UsersService, private router: Router,
-    private stateService: StateService ) { }
 
-  ngOnInit(): void {
-  }
+  constructor(
+    private _usersService: UsersService,
+    private router: Router,
+    private stateService: StateService,
+    private formBuilder: FormBuilder,
+    private _cartService: CartsService
+
+
+     ) { }
+
+     ngOnInit(): void {
+      this.userLoginForm =this.formBuilder.group({
+        userEmail: [this.loginUserData.userEmail, [Validators.required, Validators.email, Validators.maxLength(50)]],
+        password: [this.loginUserData.password, [Validators.required, Validators.minLength(6), Validators.maxLength(10)]]
+      })
+
+      combineLatest(this.userLoginForm.get('userEmail').valueChanges, this.userLoginForm.get('password').valueChanges)
+      .subscribe(p=>this.isLoginFail = false);
+
+    }
 
   onLoginClicked(): void {
-    const observable = this.usersService.login({userEmail: this.userName, password: this.password});
+    this.loginUserData = this.userLoginForm.value;
+    console.log(this.loginUserData);
+    const observable = this._usersService.login(this.loginUserData);
     observable.subscribe(
       response => {
         let loggedInUser: IUser = {
@@ -38,10 +61,22 @@ export class LoginComponent implements OnInit {
         this.stateService.shippingCity = response.shippingCity;
         this.stateService.shippingStreet = response.shippingStreet;
         this.stateService.userRole = response.role
+
+        let userDataAsString = sessionStorage.getItem("userData");
+        let userData = JSON.parse(userDataAsString);
+        console.log(userData);
+        this._cartService.cart = response.cart;
+        console.log(this._cartService.cart)
+        console.log("login response", response);
+
         this.router.navigate(['/shopping']);
       },
        error =>{
-         alert("Error: " + error);
+         alert("Password is incorrect or user name doesn't exists");
+         console.log("Login Error", error);
+
+         this.isLoginFail = true;
+
        }
     )
   }
--- client/src/app/models/SuccessfulLoginServerResponse.model.ts
+++ client/src/app/models/SuccessfulLoginServerResponse.model.ts
@@ -1,8 +1,11 @@
+import ICart from "./ICarts.model";
+
 export default interface SuccessfulLoginServerResponse{
   token: string;
   firstName: string;
   lastName: string,
   shippingCity: string,
   shippingStreet: string,
-  role: string
+  role: string,
+  cart: ICart
 }
